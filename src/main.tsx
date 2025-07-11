3159639c6c1d8bf92cf380d03b58a8c46ba3ce4c
farisfian06 <farisfian06@gmail.com>
2025-07-12 05:10:24 +0700

    feat: add toast notifications to root component

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
 import { createRoot } from "react-dom/client";
 import "./style.css";
 import Routes from "./route/Routes.tsx";
+import { ToastContainer } from "react-toastify";
 
-createRoot(document.getElementById("root")!).render(<Routes />);
+createRoot(document.getElementById("root")!).render(
+  <>
+    <ToastContainer />
+    <Routes />
+  </>
+);
